a02335e8e6e5611244e3b9699aa686815adb35e7
Lionel BERTON <lionel.berton@princeps.com>
2021-02-19 16:38:03 +0100

    First import

---
+++ src/ROTViewer/VueModele.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Princeps.Framework.Interop;
+
+namespace ROTViewer
+{
+    public class VueModele : INotifyPropertyChanged
+    {
+        private List<string> _monikers;
+
+        public List<string> Monikers
+        {
+            get
+            {
+                return _monikers;
+            }
+            set
+            {
+                _monikers = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Monikers"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RefreshTable()
+        {
+            Monikers=ROTable.Monikers.Select(x => x.Name).ToList();
+        }
+
+    }
+}
